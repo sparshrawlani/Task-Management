e80f3aabbfd4e0cf41f9825bc92d290351febafb
sparshrawlani <sparshr@ktblack.com>
2024-10-16 23:14:24 -0500

    removed extra route

--- src/index.tsx
+++ src/index.tsx
@@ -16,15 +16,6 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     children: [
       {
-        path: "status/:nameParam",
-        element: <TodoList />, // This will receive filter status as a prop
-        // Adding `filter` param for status filter
-        loader: async ({ params }) => {
-          const { filterListType } = params as { filterListType: string };
-          return { filterListType }; // Return filter type for usage
-        },
-      },
-      {
         path: "list/:nameParam",
         element: <TodoList />,
       },
